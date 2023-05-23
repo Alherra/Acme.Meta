474b1fbb69d41f5946aa25506a8999619d7c94ee
Atlas <lbsite@foxmail.com>
2023-05-23 20:48:19 +0800

    3

--- Infrastructure/Authorize.cs
+++ Infrastructure/Authorize.cs
@@ -1,5 +1,4 @@
-﻿using Meta.Contracts;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.ComponentModel;
